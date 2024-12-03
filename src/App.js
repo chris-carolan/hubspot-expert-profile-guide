d59991c83d7c8da4dd0d937c196d82fbe4851940
chris-carolan <chris.carolan@conveyingyourmessage.com>
2024-12-02 21:39:43 -0600

    Add React application files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileGuide from './components/ProfileGuide';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <ProfileGuide />
+    </div>
+  );
+}
+
+export default App;
